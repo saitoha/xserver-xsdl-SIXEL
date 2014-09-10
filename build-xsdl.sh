85cd4766a562b9354e05d7726c7c89162636f169
Hayaki Saito <user@zuse.jp>
2014-09-10 17:39:24 +0900

    Merge some parts of xserver-xsdl(https://github.com/pelya/xserver-xsdl)

---
+++ build-xsdl.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+x11prefix=/opt/X11
+
+env SDL=yes CFLAGS="-O3 -Ofast" \
+./autogen.sh \
+    --prefix=${x11prefix} \
+    --with-xkb-path=${x11prefix}/share/X11/xkb \
+    --with-xkb-bin-directory=${x11prefix}/bin \
+    --enable-debug \
+    --disable-xorg \
+    --disable-dmx \
+    --disable-xvfb \
+    --disable-xnest \
+    --disable-xquartz \
+    --disable-xwin \
+    --disable-xephyr \
+    --disable-xfake \
+    --disable-xfbdev \
+    --disable-unit-tests \
+    --enable-mitshm \
+    --disable-dri \
+    --disable-dri2 \
+    --disable-dri3 \
+    --disable-present \
+    --disable-glx \
+    --disable-xf86vidmode \
+    --disable-xquartz \
+    --disable-composite \
+    --disable-xv \
+    --disable-dga \
+    --disable-screensaver \
+    --disable-xdmcp \
+    --disable-xdm-auth-1 \
+    --disable-xinerama \
+    --disable-docs \
+    --enable-xsdl \
+    --enable-kdrive \
+    --disable-kdrive-kbd \
+    --disable-kdrive-mouse \
+    --disable-kdrive-evdev \
+
+nice -n19 make -j8
+
+# run:
+# hw/kdrive/sdl/Xsdl :1 2>/dev/null
